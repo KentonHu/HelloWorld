07f75a4253c46d895e8794bb8a7b4ff101b787ad
Kenton <hutianyu0716@gmail.com>
2022-02-06 17:28:16 +0800

    Create Repository

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+* Java程序中最基本的组成单位是类
+* 类的定义格式：
+* 	public class 类名{
+*
+* 	}
+*
+* 	这是我定义的HelloWorld类
+*
+* */
+public class HelloWorld {
+	/*
+		这是main方法
+		main方法是程序的入口方法，代码的执行是从main方法开始的
+	 */
+	public static void main(String[] args){
+		// 这是输出句，双引号里的内容是可以改变的
+		System.out.println("HelloWorld");
+		System.out.println("您好");
+	}
+}
